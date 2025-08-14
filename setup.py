e054140ac4e166091df0c4d27724bf007305f9c0
HaN <102043262+XZ-HaN@users.noreply.github.com>
2025-08-14 18:49:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LEP",
-    version="0.1.0",             
+    version=__version__,             
     packages=find_packages(),   
     description="LEP",  
     long_description=open('README.md').read(),
